8faef95acc4900c81c49ab8414f030102ae894e0
ec363 <51139488+ec363@users.noreply.github.com>
2024-10-13 15:27:48 +0100

    tidied get_properties() function
    cleaned up error checks and tidied function to avoid repetitive code

--- R/get_properties.R
+++ R/get_properties.R
@@ -33,41 +33,23 @@ get_properties <- function(slug, verbose = TRUE, save_file = FALSE, outfolder = 
 
   ## Get FP properties -----------------------------------------------------------------------------------------------------------
 
+  all_fp_df <- NULL ###
+  source <- NULL
+
+  # 1) fpbase version ---
+
   # In case of FPbase url error, wrap FPbase call in tryCatch
   tryCatch(
     expr = {
 
-      all_fp_df <- "empty"
+      # all_fp_df <- "empty" ###
 
-      # 1) fpbase version ---
       # All FP properties
       all_fp_df <- jsonlite::fromJSON("https://www.fpbase.org/api/proteins/?format=json")
-
-      # Get FP entry
-      fp_entry <- all_fp_df %>%
-        dplyr::filter(slug == {{slug}})
-      fp_entry
-
-      # FP properties
-      fp_properties <- fp_entry[[11]][[1]]
-      fp_properties
-
       if(verbose){
-        message("FP properties retrieved from FPbase:")
-        print(fp_properties)
+        message("FP data retrieved from FPbase.")
       }
-
-      # Warning mesg for proteins with multiple states
-      if(nrow(fp_properties)>1){
-        message("FP has multiple states, taking first:")
-        fp_properties <- fp_properties[1,]
-        print(fp_properties)
-      }
-      if(nrow(fp_properties)<1){
-        return(message("Error: no entries matching slug in FPbase."))
-      }
-      # fpbase version ---
-
+      source <- "fpbase"
 
     },
     error = function(e){
@@ -87,40 +69,50 @@ get_properties <- function(slug, verbose = TRUE, save_file = FALSE, outfolder = 
   )
 
   # If FPbase download fails, use package data instead:
+
   # 2) pre-downloaded version ---
-  if(all_fp_df == "empty"){
+
+  # if(all_fp_df == "empty"){ # ### causes persistent error Error in if (all_fp_df == "empty") { : the condition has length > 1
+  if(is.null(all_fp_df)){ ###
 
     # All FP properties - from pre-downloaded data
     all_fp_df <- fpcountr::fpbase_data
     all_fp_df
 
-    # Get FP entry
-    fp_entry <- all_fp_df %>%
-      dplyr::filter(slug == {{slug}})
-    fp_entry
-
-    # FP properties
-    # fp_properties <- fp_entry[[11]][[1]] # don't need this here
-    fp_properties <- fp_entry
-    fp_properties
-
     if(verbose){
-      message("FP properties retrieved from FPbase data stored in package:")
-      print(fp_properties)
+      message("FP data retrieved from FPbase data stored in package.")
     }
-
-    # Warning mesg for proteins with multiple states
-    if(nrow(fp_properties)>1){
-      message("FP has multiple states, taking first:")
-      fp_properties <- fp_properties[1,]
-      print(fp_properties)
-    }
-    if(nrow(fp_properties)<1){
-      return(message("Error: no entries matching slug in FPbase."))
-    }
+    source <- "fpcountr"
 
   }
-  # pre-downloaded version ---
+
+  # Get FP entry
+  fp_entry <- all_fp_df %>%
+    dplyr::filter(slug == {{slug}})
+  fp_entry
+
+  # Error message for slugs with no entries
+  if(nrow(fp_entry)<1){
+    return(message("Error: no entries matching slug in database."))
+  }
+
+  # FP properties
+  if(source == "fpbase"){
+    fp_properties <- fp_entry[[11]][[1]]
+    fp_properties
+  } else {
+    fp_properties <- fp_entry
+    fp_properties
+  }
+
+  # Warning mesg for proteins with multiple states
+  if(nrow(fp_properties)>1){
+    message("FP has multiple states:")
+    print(fp_properties)
+    message("..taking first:")
+    fp_properties <- fp_properties[1,]
+    print(fp_properties)
+  }
 
   # Save and return ------------------------------
 
